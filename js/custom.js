059228f3101e5d6afa0499da658062563e512624
Shamim Islam <64851056+Shamim-Islam@users.noreply.github.com>
2020-07-12 17:31:27 +0600

    Code & Components
    Creative website code and components

---
+++ js/custom.js
@@ -0,0 +1,90 @@
+// Highlight active menu on click //
+
+$(document).ready(function () {
+	'use strict';
+	
+	$('.navbar-nav li a').click(function () {
+		'use strict';
+		
+		$('.navbar-nav li a').parent().removeClass('active');
+		$(this).parent().addClass('active');
+		
+	});
+	
+});
+
+// highlight active menu on scroll //
+
+$(document).ready(function () {
+	'use strict';
+	
+	$(window).scroll(function () {
+		'use strict';
+		
+		$('section, header').each(function () {
+			'use strict';
+			
+			var bb = $(this).attr('id'); // its define the id of each section
+			var hei = $(this).outerHeight();
+			var grttop = $(this).offset().top - 50;
+			
+			if ($(window).scrollTop() > grttop && $(window).scrollTop() < grttop + hei) {
+				
+				$('.navbar-nav li a[href="#' + bb + '"]').parent().addClass('active');
+				
+			} else {
+				
+				$('.navbar-nav li a[href="#' + bb + '"]').parent().removeClass('active');
+				
+			}
+			
+		});
+		
+	});
+		
+});
+
+
+/*----------- Smooth scrolling --------------*/
+
+$(document).ready(function () {
+
+	'use strict';
+
+	$('.nav-link, .navbar-brand').click(function () {
+		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') ||
+			location.hostname === this.hostname) {
+
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
+
+});
+
+/*----------- Add counter up --------------*/
+
+$(document).ready(function () {
+	'use strict';
+	
+	$('.counter-num').counterUp({
+		delay: 10,
+		time: 2000
+	});
+
+});
+
+/*----------- Add animations & initialize wow --------------*/
+
+$(document).ready(function () {
+
+	'use strict';
+	new WOW().init();
+
+});
